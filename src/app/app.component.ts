554efedda834335ae6d549da7ad15e8d31bf6c5b
pavelp <pavel.petrov@asseco.lt>
2016-10-06 17:52:49 +0300

    app -> gs

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
+  selector: 'gs-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
